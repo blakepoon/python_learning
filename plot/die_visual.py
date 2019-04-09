75fd0507b12e8c0e6630ceb392a6bc698c766482
Bin Pan <blakebp@Bins-MacBook.local>
2019-04-09 23:24:44 +0800

    csv

--- plot/die_visual.py
+++ plot/die_visual.py
@@ -1,20 +1,21 @@
 import pygal
 from die import Die
 
-die = Die()
+die_1 = Die()
+die_2 = Die()
 results = []
 for roll_num in range(1000):
-	results.append(die.roll())
+	results.append(die_1.roll() + die_2.roll())
 
 frequencies = []
-for value in range(1, die.num_sides+1):
+for value in range(2, die_1.num_sides+die_2.num_sides+1):
 	frequency = results.count(value)
 	frequencies.append(frequency)
 
 hist = pygal.Bar()
 
 hist.title = "Results of rolling one D6 1000 times"
-hist.x_labels = ['1', '2', '3', '4', '5', '6']
+hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
 hist.x_title = 'Result'
 hist.y_title = 'frequency of Result'
 
